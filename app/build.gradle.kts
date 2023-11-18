d57ba21b9e4927e6d7f38243b04c8c5973f5e5dd
abaferas <wasfyelgamed@gmailcom>
2023-11-18 14:50:01 +0200

    add libraries and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -22,7 +25,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -40,7 +43,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -50,20 +53,38 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(Deps.coreKtx)
+    implementation(Deps.lifecycleRuntimeKtx)
+    implementation(Deps.activityCompose)
+    implementation(platform(Deps.composeBom))
+    implementation(Deps.composeUI)
+    implementation(Deps.composeUIGraphics)
+    implementation(Deps.composeUIToolingPreview)
+    implementation(Deps.composeMaterial3)
+    testImplementation(Deps.junit)
+    androidTestImplementation(Deps.junitExt)
+    androidTestImplementation(Deps.espresso)
+    androidTestImplementation(platform(Deps.composeUIGraphics))
+    androidTestImplementation(Deps.composeUIGraphics)
+    debugImplementation(Deps.composeUIGraphics)
+    debugImplementation(Deps.composeUIGraphics)
+    androidTestImplementation(platform(Deps.composeBom))
+    androidTestImplementation(Deps.composeUITestJunit4)
+    debugImplementation(Deps.composeUITooling)
+    debugImplementation(Deps.composeUITestManifest)
+    implementation(Deps.appCompat)
+    implementation(Deps.hiltAndroid)
+    kapt(Deps.hiltCompiler)
+    implementation(Deps.hiltNavigationCompose)
+    implementation(Deps.lifecycleViewModelKts)
+    implementation(Deps.lifecycleViewModelCompose)
+    implementation(Deps.lifecycleRunTimeCompose)
+    implementation(Deps.lifecycleSavedState)
+    implementation(Deps.lifecycleCommon)
+    implementation(Deps.lifecycleService)
+    implementation(Deps.lifecycleProcess)
+    testImplementation(Deps.lifecycleRuntimeTesting)
+}
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,30 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    val navVersion by extra ("2.6.0")
+
+    repositories{
+        google()
+        mavenCentral()
+        maven{
+            url  = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+    }
+    dependencies{
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
+        classpath ("com.google.gms:google-services:4.4.0")
+        classpath ("com.google.firebase:perf-plugin:1.4.2")
+        classpath ("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+    }
+}
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    kotlin("kapt") version "1.9.0"
 }
